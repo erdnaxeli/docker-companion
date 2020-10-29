adc108bd665b092a8d0117595a3e3af5359d1c30
Alexandre Morignot <amorignot@meilleursagents.com>
2020-10-29 23:50:12 +0100

    Read config.yaml and connect to Matrix

--- src/docker-companion.cr
+++ src/docker-companion.cr
@@ -4,12 +4,50 @@ require "./manager"
 
 require "caridina"
 
+require "file"
 require "option_parser"
+require "yaml"
 
 module Companion
   VERSION = "0.1.0"
 
+  struct Config
+    include YAML::Serializable
+
+    struct Matrix
+      include YAML::Serializable
+
+      property homeserver : String
+      property access_token : String
+    end
+
+    property matrix : Matrix
+    property users : Array(String)
+  end
+
+  def self.read_config(filename = "config.yaml") : Config
+    content = File.read(filename)
+    Config.from_yaml(content)
+  end
+
   def self.run : Nil
+    config = read_config
+
+    conn = Caridina::ConnectionImpl.new(
+      config.matrix.homeserver,
+      config.matrix.access_token,
+    )
+    channel = Channel(Caridina::Events::Sync).new
+    conn.sync(channel)
+
+    loop do
+      sync = channel.receive
+      sync.room_events do |event|
+        if (message = event.message?) && event.sender != conn.user_id && config.users.includes? event.sender
+          conn.send_message(event.room_id, message.body)
+        end
+      end
+    end
     # url = ""
     # access_token = ""
 
@@ -31,27 +69,38 @@ module Companion
     #   conn.send_message("!IZjjATKJFYVLSVzyMP:cervoi.se", "Hello, world!")
     # end
 
-    c = Docker::Client.new(Docker::Client::Config.new)
-    puts c.containers(true)
+    c = Manager.new(Docker::Client::Local.new)
+    c.add_project(
+      "bash",
+      %(
+        version: "3.8"
+        services:
+          test:
+            image: wardsco/sleep
+      )
+    )
+    c.up("bash")
 
-    c.pull_image("bash") do |r|
-      puts r
-    end
+    # c.pull_image("bash") do |r|
+    #   puts r
+    # end
 
-    puts "Creating bash container"
-    o = Docker::CreateContainerOptions.new
-    o.image = "bash"
-    mount = Docker::CreateContainerOptions::HostConfig::Mount.new
-    mount.target = "/tmp/home"
-    mount.source = "/home"
-    o.host_config.mounts << mount
-    o.cmd << "sleep" << "20"
-    r = c.create_container(o)
-    puts r.id, r.warnings, r.message
-    puts c.containers
+    # puts "Creating bash container"
+    # o = Docker::CreateContainerOptions.new
+    # o.image = "bash"
+    # mount = Docker::CreateContainerOptions::HostConfig::Mount.new
+    # mount.target = "/tmp/home"
+    # mount.source = "/home"
+    # o.host_config.mounts << mount
+    # o.cmd << "sleep" << "20"
+    # r = c.create_container(o)
+    # puts r.id, r.warnings, r.message
+    # puts c.containers
 
-    if id = r.id
-      c.start_container(id)
-    end
+    # if id = r.id
+    #   c.start_container(id)
+    # end
   end
 end
+
+Companion.run
